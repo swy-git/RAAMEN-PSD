1434d813b5267b74d7947380aa15f711be6f8bd7
San <istamarahsan@gmail.com>
2023-03-13 12:21:04 +0700

    add global http client

--- PSD-Project/global.asax.cs
+++ PSD-Project/global.asax.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
@@ -6,6 +7,7 @@ namespace PSD_Project
 {
     public class RaamenApp : HttpApplication
     {
+        public static readonly HttpClient HttpClient = new HttpClient();
         protected void Application_Start(object sender, EventArgs e) 
         {
             var config = GlobalConfiguration.Configuration;
